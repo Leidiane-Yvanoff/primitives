1c0af4ef1b350d51c16b657dfd1052675feb0edd
Leidiane <leidiane.yvanoff@gmail.com>
2019-10-17 20:45:02 +0200

    Primitives

---
+++ src/exercices/PrimitiveTraining.java
@@ -0,0 +1,52 @@
+public class PrimitiveTraining  { 
+
+        public static int integer() {
+	        int integerTest = 42;
+	        return integerTest;
+	    }
+        public static String str() {
+        	String helloTest = "Hello";
+        	return helloTest;
+        }
+        public static double real() {
+        	double realTest = 3.14;
+        	return realTest;
+        }
+       
+        public static boolean bool() {
+        	boolean boolTest = false;
+        	return boolTest;
+        }
+        public static String nothing() {
+        	String nothingTest = null;
+        	return nothingTest;
+        }
+        
+        
+        
+
+	    public static void main(String[] args ) {
+		
+		
+	    System.out.println(integer());
+	    System.out.println(str());
+	    System.out.println(real());
+	    System.out.println(bool());
+	    System.out.println(nothing());
+	   
+	    
+	}   
+
+	    
+} 	
+
+
+// TODO 3 : Create a method 'real', result expected : double 3.14
+
+
+// TODO 4 : Create a method 'bool', result expected : boolean false
+
+
+// TODO 5 : Create a method 'nothing', no result expected
+
+
